90844469a994e6ece82337f9e43723b497c645c5
Crassirostris <crassirostris@yandex.com>
2014-11-10 00:33:54 +0500

    markup

--- server/personal/urls.py
+++ server/personal/urls.py
@@ -3,5 +3,8 @@ from django.conf.urls import url
 from personal import views
 
 urlpatterns = [
+    url(r'^about$', views.about, name='about'),
+    url(r'^contacts$', views.contacts, name='contacts'),
+    url(r'^gallery$', views.gallery, name='gallery'),
     url(r'^$', views.index, name='index'),
 ]
--- server/personal/views.py
+++ server/personal/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
 
 def index(request):
+    print('index')
     return render(request, 'index.html')
+
+
+def about(request):
+    print('about')
+    return render(request, 'about.html')
+
+
+def contacts(request):
+    return render(request, 'contacts.html')
+
+
+def gallery(request):
+    return render(request, 'gallery.html')
