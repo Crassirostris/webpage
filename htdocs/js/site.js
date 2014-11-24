ec6f1aa5c2e7c4965b985beee0b396f4592c1606
Crassirostris <crassirostris@yandex.com>
2014-11-25 00:39:22 +0500

    Loading

--- htdocs/js/site.js
+++ htdocs/js/site.js
@@ -1,23 +1,36 @@
+function adjustWidthTo(imageSelector) {
+    var image = $(imageSelector);
+    console.log(image);
+    var width = image[0].naturalWidth;
+    if (width == undefined) {
+        var tempImage = new Image();
+        tempImage.src = this.src;
+        width = tempImage.width;
+    }
+    console.log(width);
+    $('.modal-image-container').width(width);
+    $('.modal-content').width(width);
+    image.width(width - 20);
+}
+
 $(function(){
     var modalImage = document.getElementById("modalImage");
     if (modalImage != null) {
         document.getElementById("modalImage").onload = function () {
-            var width = document.getElementById("modalImage").naturalWidth;
-            if (width == undefined) {
-                var image = new Image();
-                image.src = this.src;
-                width = image.width;
-            }
-            $('.modal-image-container').width(width);
-            $('.modal-content').width(width);
-            $('#modalImage').width(width - 20);
+            adjustWidthTo('#modalImage');
+            $('#loadingImage').hide();
+            $('#modalImage').show();
         };
     }
 
     $('.image-preview').click(function () {
         var href = $(this).attr('full-image');
-        $('#modalImage').width(10000);
         $('#modalImage').attr('src', href);
+
+        $('#modalImage').hide();
+        $('#loadingImage').show();
+        adjustWidthTo('#loadingImage');
+
         $('#imageModal').modal();
     });
 });
